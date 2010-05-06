8d36bd84bb03da45706f681f72a8e8f77bc631fa
Nat Budin <natbudin@gmail.com>
2010-05-06 09:36:55 -0400

    Fix module path

--- lib/devise_cas_authenticatable/routes.rb
+++ lib/devise_cas_authenticatable/routes.rb
@@ -1,12 +1,12 @@
+require 'action_dispatch/routing/mapper'
+
 module ActionDispatch::Routing
-  class RouteSet
-    class Mapper
+  class Mapper
     protected
   
-      def devise_cas(mapping, controllers)
-        scope mapping.full_path do
-          get mapping.path_names[:sign_out], :to => "#{controllers[:cas]}#destroy", :as => :"destroy_#{mapping.name}_session"
-        end
+    def devise_cas(mapping, controllers)
+      scope mapping.full_path do
+        get mapping.path_names[:sign_out], :to => "#{controllers[:cas]}#destroy", :as => :"destroy_#{mapping.name}_session"
       end
     end
   end
