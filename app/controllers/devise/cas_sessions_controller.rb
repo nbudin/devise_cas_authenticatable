9cbed8e521eb0b35d4808c2489649a7f36d87a3f
Nat Budin <natbudin@gmail.com>
2010-07-01 15:09:46 -0400

    Spell name correctly

--- app/controllers/devise/cas_sessions_controller.rb
+++ app/controllers/devise/cas_sessions_controller.rb
@@ -1,10 +1,10 @@
-class CasSessionsController < ApplicationController
+class Devise::CasSessionsController < ApplicationController
   prepend_before_filter :require_no_authentication, :only => [:login]
   include Devise::Controllers::InternalHelpers
   
   def service
     if signed_in?(resource_name)
-      redirect_to after_sign_in_path(resource_name)
+      redirect_to after_sign_in_path_for(resource_name)
     else
       redirect_to root_url
     end
