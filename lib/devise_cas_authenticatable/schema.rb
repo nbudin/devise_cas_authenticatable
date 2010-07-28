278a0948436a3b8905dd7fc0421393f19e123bc8
Nat Budin <natbudin@gmail.com>
2010-07-28 13:36:48 -0400

    Support legacy Devise API

--- lib/devise_cas_authenticatable/schema.rb
+++ lib/devise_cas_authenticatable/schema.rb
@@ -3,7 +3,11 @@ require 'devise/schema'
 module Devise
   module Schema
     def cas_authenticatable
-      apply_devise_schema :username, String
+      if respond_to?(:apply_devise_schema)
+        apply_devise_schema :username, String
+      else
+        apply_schema :username, String
+      end
     end
   end
 end
