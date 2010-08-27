1e0de875385c90c1b790116c05cf572ae769a5f9
Nat Budin <natbudin@gmail.com>
2010-08-27 10:34:18 -0400

    Fix path in Rails 2

--- lib/devise_cas_authenticatable/routes.rb
+++ lib/devise_cas_authenticatable/routes.rb
@@ -23,7 +23,7 @@ else
     
     def cas_authenticatable(routes, mapping)
       routes.with_options(:controller => 'devise/cas_sessions', :name_prefix => nil) do |session|
-        session.connect(mapping.raw_path, :action => 'service', :conditions => {:method => :get})
+        session.connect('/', :action => 'service', :conditions => {:method => :get})
         session.send(:"new_#{mapping.name}_session", mapping.path_names[:sign_in], :action => 'create', :conditions => {:method => :get})
         session.send(:"#{mapping.name}_session", mapping.path_names[:sign_in], :action => 'create', :conditions => {:method => :post})
         session.send(:"destroy_#{mapping.name}_session", mapping.path_names[:sign_out], :action => 'destroy', :conditions => { :method => :get })
