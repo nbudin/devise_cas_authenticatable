ff491838b0401f1c03d849a68117430233459644
Nat Budin <natbudin@gmail.com>
2011-05-11 11:49:09 -0400

    Version 1.0.0.alpha8 to fix a Rails 2-only typo

--- lib/devise_cas_authenticatable/routes.rb
+++ lib/devise_cas_authenticatable/routes.rb
@@ -25,7 +25,7 @@ else
     def cas_authenticatable(routes, mapping)
       routes.with_options(:controller => 'devise/cas_sessions', :name_prefix => nil) do |session|
         session.send(:"#{mapping.name}", '/', :action => 'service', :conditions => {:method => :get})
-        session.send(:"unregistered_#{mapping_name}_session", '/unregistered', :action => "unregistered", :conditions => {:method => :get})
+        session.send(:"unregistered_#{mapping.name}_session", '/unregistered', :action => "unregistered", :conditions => {:method => :get})
         session.send(:"new_#{mapping.name}_session", mapping.path_names[:sign_in], :action => 'new', :conditions => {:method => :get})
         session.send(:"#{mapping.name}_session", mapping.path_names[:sign_in], :action => 'create', :conditions => {:method => :post})
         session.send(:"destroy_#{mapping.name}_session", mapping.path_names[:sign_out], :action => 'destroy', :conditions => { :method => :get })
