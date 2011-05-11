c9e8333a2157fffd319bfc378f02ed0da6591466
Raphael Randschau <nicolai86@me.com>
2011-05-11 22:06:27 +0200

    add mattr_accessor for cas_username_column

--- lib/devise_cas_authenticatable.rb
+++ lib/devise_cas_authenticatable.rb
@@ -41,7 +41,7 @@ module Devise
   # Name of the parameter passed in the logout query 
   @@cas_destination_logout_param_name = nil
   
-  mattr_accessor :cas_base_url, :cas_login_url, :cas_logout_url, :cas_validate_url, :cas_create_user, :cas_destination_logout_param_name
+  mattr_accessor :cas_base_url, :cas_login_url, :cas_logout_url, :cas_validate_url, :cas_create_user, :cas_destination_logout_param_name, :cas_username_column
 
   def self.cas_create_user?
     cas_create_user
