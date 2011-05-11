54dbe11138b128696c11194681290580641ef8c4
Raphael Randschau <nicolai86@me.com>
2011-05-11 21:58:12 +0200

    update model to user dynamic username columnts

--- lib/devise_cas_authenticatable/model.rb
+++ lib/devise_cas_authenticatable/model.rb
@@ -19,7 +19,7 @@ module Devise
           ::Devise.cas_client.validate_service_ticket(ticket) unless ticket.has_been_validated?
           
           if ticket.is_valid?
-            conditions = {:email => ticket.response.user}
+            conditions = {::Devise.cas_username_column => ticket.response.user}
             
             # We don't want to override Devise 1.1's find_for_authentication
             resource = if respond_to?(:find_for_authentication)
