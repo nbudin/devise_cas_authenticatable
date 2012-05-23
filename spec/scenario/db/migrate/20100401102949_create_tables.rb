daa19ed58c1378bb23800a72d937412d18aa50dd
Nat Budin <natbudin@gmail.com>
2012-05-23 14:13:09 -0400

    Switch schema to work with Devise 2.1

--- spec/scenario/db/migrate/20100401102949_create_tables.rb
+++ spec/scenario/db/migrate/20100401102949_create_tables.rb
@@ -1,11 +1,13 @@
 class CreateTables < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-      t.cas_authenticatable
-      t.rememberable
+      t.string :username, :null => false
+      t.datetime :remember_created_at
       t.string :email
       t.timestamps
     end
+
+    add_index :users, :username, :unique => true
   end
 
   def self.down
