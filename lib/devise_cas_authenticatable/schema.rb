44aacb23fb5b4cc9d22434c952b9d1d88fe28e88
Nat Budin <natbudin@gmail.com>
2012-05-23 14:04:07 -0400

    Only do schema stuff if running on Devise < 2.1 (closes #49)

--- lib/devise_cas_authenticatable/schema.rb
+++ lib/devise_cas_authenticatable/schema.rb
@@ -1,14 +1,19 @@
-require 'devise/schema'
+require 'devise/version'
 
-module Devise
-  module Schema
-    # Adds the required fields for cas_authenticatable to the schema.  Currently
-    # this is just username (String).
-    def cas_authenticatable
-      if respond_to? :apply_devise_schema
-        apply_devise_schema :username, String
-      else
-        apply_schema :username, String
+# Devise 2.1 removes schema stuff
+if Devise::VERSION < "2.1"
+  require 'devise/schema'
+
+  module Devise
+    module Schema
+      # Adds the required fields for cas_authenticatable to the schema.  Currently
+      # this is just username (String).
+      def cas_authenticatable
+        if respond_to? :apply_devise_schema
+          apply_devise_schema :username, String
+        else
+          apply_schema :username, String
+        end
       end
     end
   end
