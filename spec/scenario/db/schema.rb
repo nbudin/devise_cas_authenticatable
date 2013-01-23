9101522da61f8a66367287e8933f12129174ee9d
Nat Budin <nbudin@patientslikeme.com>
2013-01-23 11:34:31 -0500

    Regen schema

--- spec/scenario/db/schema.rb
+++ spec/scenario/db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,16 +11,27 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111002012903) do
+ActiveRecord::Schema.define(:version => 20121009092400) do
 
   create_table "sessions", :force => true do |t|
     t.string   "session_id", :null => false
     t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
+  create_table "users", :force => true do |t|
+    t.string   "username",            :null => false
+    t.datetime "remember_created_at"
+    t.string   "email"
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
+    t.boolean  "deactivated"
+  end
+
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
