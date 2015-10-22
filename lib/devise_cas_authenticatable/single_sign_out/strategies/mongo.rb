2c0bdaed77ebe36e6a35627de962bb76b9c48017
Tomás Charad <tomas.charad@gmail.com>
2015-10-22 11:45:02 -0300

    Adding specific logs for strategy

--- lib/devise_cas_authenticatable/single_sign_out/strategies/mongo.rb
+++ lib/devise_cas_authenticatable/single_sign_out/strategies/mongo.rb
@@ -3,27 +3,23 @@ module DeviseCasAuthenticatable
     module Strategies
       class Mongo < Base
           def store_session_id_for_index(session_index, session_id)
-            logger.debug("Storing #{session_id} for index #{session_index}")
+            logger.debug("Storing #{session_id} for index #{session_index} in mongo")
             CasSession.create(cas_session_index: session_index, session_id: session_id)
           end
 
           def find_session_id_by_index(session_index)
             sid = CasSession.find_by(cas_session_index: session_index).session_id
-            logger.debug("Found session id #{sid} for index #{session_index}")
+            logger.debug("Found session id #{sid} for index #{session_index} in mongo")
             sid
           end
 
           def delete_session_index(session_index)
-            logger.debug("Deleting index #{session_index}")
+            logger.debug("Deleting index #{session_index} in mongo")
             # TCT: Not sure if more than one regiter was created, so deleting them all
             CasSession.where(cas_session_index: session_index).destroy_all
           end
 
           private
-
-          def cache_key(session_index)
-            "devise_cas_authenticatable:#{session_index}"
-          end
       end
     end
   end
