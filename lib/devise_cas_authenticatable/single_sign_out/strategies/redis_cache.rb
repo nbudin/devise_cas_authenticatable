d91e261ed76ed06317a2f76a63fae6a2bf638364
Joel Cornelius <joel@matasano.com>
2016-06-21 11:50:31 -0500

    Attempt at making devise-cas sso read sessions out of redis

--- lib/devise_cas_authenticatable/single_sign_out/strategies/redis_cache.rb
+++ lib/devise_cas_authenticatable/single_sign_out/strategies/redis_cache.rb
@@ -1,20 +1,26 @@
 module DeviseCasAuthenticatable
   module SingleSignOut
     module Strategies
-      class RailsCache < Base
+      class RedisCache < Base
+        include DeviseCasAuthenticatable::SingleSignOut::DestroySession
+current_session_store.inst
         def store_session_id_for_index(session_index, session_id)
           logger.debug("Storing #{session_id} for index #{session_index}")
-          Rails.cache.write(cache_key(session_index), session_id)
+          current_session_store.instance_variable_get(:@pool).set(
+            cache_key(session_index),
+            session_id
+          )
         end
         def find_session_id_by_index(session_index)
-          sid = Rails.cache.read(cache_key(session_index))
+          sid = current_session_store.instance_variable_get(:@pool).get(cache_key(session_index))
           logger.debug("Found session id #{sid} for index #{session_index}")
           sid
         end
         def delete_session_index(session_index)
           logger.debug("Deleting index #{session_index}")
-          Rails.cache.delete(cache_key(session_index))
+          destroy_session_by_id(session_index)
         end
+
         private
         def cache_key(session_index)
           "devise_cas_authenticatable:#{session_index}"
@@ -24,4 +30,4 @@ module DeviseCasAuthenticatable
   end
 end
 
-::DeviseCasAuthenticatable::SingleSignOut::Strategies.add(:rails_cache, DeviseCasAuthenticatable::SingleSignOut::Strategies::RailsCache )
+::DeviseCasAuthenticatable::SingleSignOut::Strategies.add(:redis_cache, DeviseCasAuthenticatable::SingleSignOut::Strategies::RedisCache )
